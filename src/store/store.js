fea522df4e5148959c8cdbc1b7f22773d2765468
parthakundu3 <partha.kundu3@gmail.com>
2025-05-24 21:15:52 +0530

    Git merged as current branch behind the 4 commits

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../feature/counterSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
